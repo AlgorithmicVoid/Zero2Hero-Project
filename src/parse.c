97f3f9d570c41fd9d232519cfa60bb509718bd10
AlgorithmicVoid <voidlordgames@gmail.com>
2025-11-20 16:18:09 +1100

    Initial commit with command line args, initial file creation and validation and output functionality.

--- src/parse.c
+++ src/parse.c
@@ -9,7 +9,7 @@
 #include "parse.h"
 
 
-int output_file(int fd, struct dbheader_t *dbhdr){
+void output_file(int fd, struct dbheader_t *dbhdr){
     if (fd <0){
         printf("Got a bad FD from the user\n");
         return STATUS_ERROR;
