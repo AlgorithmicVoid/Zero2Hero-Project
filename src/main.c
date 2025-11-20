4c6441b2ee4fc9382dbcfea17337d67de5caf30a
AlgorithmicVoid <voidlordgames@gmail.com>
2025-11-20 16:14:51 +1100

    Initial commit with command line args, initial file creation and validation and output functionality.

--- src/main.c
+++ src/main.c
@@ -7,67 +7,68 @@
 #include "parse.h"
 
 void print_usage(char *argv[]){
-    printf("Usage: %s -n -f <database file>\n", argv[0]);
-    printf("\t -n - Create a new database file\n");
-    printf("\t -f - Path to Database file\n");
-    return;
+	printf("Usage: %s -n -f <database file>\n", argv[0]);
+	printf("\t -n - Create a new database file\n");
+	printf("\t -f - Path to Database file\n");
+	return;
 }
 
 int main(int argc, char *argv[]){
 
-    char *filepath = NULL;
-    bool newfile = false;
-    int c;
-    int dbfd = -1;
-    struct dbheader_t *dbhdr = NULL;
+	char *filepath = NULL;
+	bool newfile = false;
+	int c;
+	int dbfd = -1;
+	struct dbheader_t *dbhdr = NULL;
 
-    while ((c = getopt(argc, argv, "nf:")) != -1){
-        switch(c) {
-            case 'n':
-                newfile = true;
-                break;
-            case 'f':
-                filepath = optarg;
-                break;
-            case '?':
-                printf("Unknown option -%c\n", c);
-                break;
-            default:
-                return -1;
-        }
-    }
+	while ((c = getopt(argc, argv, "nf:")) != -1){
+		switch(c) {
+			case 'n':
+				newfile = true;
+				break;
+			case 'f':
+				filepath = optarg;
+				break;
+			case '?':
+				printf("Unknown option -%c\n", c);
+				break;
+			default:
+				return -1;
+		}
+	}
 
-    if (filepath == NULL){
-        printf("Filepath is a required argument\n");
-        print_usage(argv);
-        return 0;
-    }
+	if (filepath == NULL){
+		printf("Filepath is a required argument\n");
+		print_usage(argv);
+		return 0;
+	}
 
-    if (newfile){
-        dbfd = create_db_file(filepath);
-        if (dbfd == STATUS_ERROR){
-            printf("Failed to create new database file\n");
-            return -1;
-        }
+	if (newfile){
+		dbfd = create_db_file(filepath);
+		if (dbfd == STATUS_ERROR){
+			printf("Failed to create new database file\n");
+			return -1;
+		}
 
-        if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR){
-            printf("Failed to create database header\n");
-            return -1;
-        }
-    } else {
-        dbfd = open_db_file(filepath);
-        if (dbfd == STATUS_ERROR){
-            printf("Failed to open database file\n");
-            return -1;
-        }
+		if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR){
+			printf("Failed to create database header\n");
+			return -1;
+		}
+	} else {
+		dbfd = open_db_file(filepath);
+		if (dbfd == STATUS_ERROR){
+			printf("Failed to open database file\n");
+			return -1;
+		}
 
-        if(validate_db_header(dbfd, &dbhdr) == STATUS_ERROR){
-            printf("Failed to validate database header\n");
-            return -1;
-        }
-    }
+		if(validate_db_header(dbfd, &dbhdr) == STATUS_ERROR){
+			printf("Failed to validate database header\n");
+			return -1;
+		}
+	}
 
-    output_file(dbfd, dbhdr);
 
-    return 0;
+	output_file(dbfd, dbhdr);
+
+	return 0;
 }
